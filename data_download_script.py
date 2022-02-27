c5f0345c15ebe92d37c87b8d9f22418147196e47
tuannt2 <tuantruong5770@gmail.com>
2022-02-27 15:21:50 -0800

    added data processing script

--- data_download_script.py
+++
@@ -1,8 +0,0 @@
-import tensorflow_datasets as tfds
-
-
-if __name__ == '__main__':
-    datasets = ['billsum', 'cnn_dailymail']
-    for dataset in datasets:
-        tfds.load(dataset, data_dir='./data', split='train')
-
--- main.py
+++ main.py
@@ -1,2 +1,11 @@
+import tensorflow as tf
+
+
+def test_built_with_gpu():
+    print("Tensorflow built with CUDA:", tf.test.is_built_with_cuda())
+    print(tf.config.list_physical_devices('GPU'))
+
+
 if __name__ == '__main__':
     pass
+
